cdc66d6e24b4cba36c2acac1ba374dc5f846754b
Christian Sciberras <christian@sciberras.me>
2015-04-12 17:41:54 +0200

    Initial Commit

---
+++ ProgressForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GlobalCapitalTests.TestUtils.Framework
+{
+    public partial class ProgressForm : Form
+    {
+        public ProgressForm(string title)
+        {
+            InitializeComponent();
+            this.Text = title;
+        }
+
+        enum HitTest
+        {
+            Caption = 2,
+            Transparent = -1,
+            Nowhere = 0,
+            Client = 1,
+            Left = 10,
+            Right = 11,
+            Top = 12,
+            TopLeft = 13,
+            TopRight = 14,
+            Bottom = 15,
+            BottomLeft = 16,
+            BottomRight = 17,
+            Border = 18
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == 0x84) // WM_NCHITTEST
+            {
+                var result = (HitTest)m.Result.ToInt32();
+                if (result == HitTest.Top || result == HitTest.Bottom)
+                    m.Result = new IntPtr((int)HitTest.Caption);
+                if (result == HitTest.TopLeft || result == HitTest.BottomLeft)
+                    m.Result = new IntPtr((int)HitTest.Left);
+                if (result == HitTest.TopRight || result == HitTest.BottomRight)
+                    m.Result = new IntPtr((int)HitTest.Right);
+            }
+        }
+    }
+}
---
+++ ProgressForm.designer.cs
@@ -0,0 +1,81 @@
+﻿namespace GlobalCapitalTests.TestUtils.Framework
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(4, 17);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(221, 16);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Running...";
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 37);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximumSize = new System.Drawing.Size(1025, 71);
+            this.MinimumSize = new System.Drawing.Size(245, 71);
+            this.Name = "ProgressForm";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Progress";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.Label label1;
+    }
+}
